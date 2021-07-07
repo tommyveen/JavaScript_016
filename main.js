a5e294fda1a19807df47d5c52fc3e84def9faa86
Tommy Veen <tommy_veen@hotmail.com>
2021-07-07 15:32:04 +0200

    arrow functions

---
+++ main.js
@@ -0,0 +1,11 @@
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+  
+ikRockArrowFunctions()
+
+const fivePlusSeven = () => 5 + 7;
+
+const myFunction = (a, b) => a + b;
+
+const addFive = number => number + 5;
+
+const ikRockArrowFunctions = () => ({Greeting: "hoi"});
